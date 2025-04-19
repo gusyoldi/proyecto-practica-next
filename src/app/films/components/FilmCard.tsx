cc5899b4b92c26c8267d85e07be2b92c5648d06c
Gustavo Yoldi <gyoldi@auronix.com>
2025-04-19 16:02:46 -0300

    replace card component

--- src/app/films/components/FilmCard.tsx
+++ src/app/films/components/FilmCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardFooter } from '@/components/ui/Card';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import { Film } from '@/lib/schemas';
 import Image from 'next/image';
 import Link from 'next/link';
